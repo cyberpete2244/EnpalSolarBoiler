b881614731594f1b6a6ad07d79daa71b5f1bec7f
cyberpete2244 <shqfaya@gmail.com>
2025-08-18 15:42:24 +0200

    Basic Files according to tutorial at https://blog.berrybase.de/relais-steuerung-mit-dem-raspberry-pi-so-funktionierts/

---
+++ relais.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
+import time
+import RPi.GPIO as GPIO
+
+pin1 = 32  # Relais Pins Nummer
+pin2 = 16
+
+GPIO.setmode(GPIO.BOARD)  # GPIO MODE
+
+GPIO.setup(pin1, GPIO.OUT)  # Alle Relais Pins als OUTPUT
+GPIO.setup(pin2, GPIO.OUT)
+
+GPIO.output(pin1, 1)
+GPIO.output(pin2, 1)
+
+app = Flask(__name__)  # Flask Server einrichten
+
+
+# Rückgabe der Seite index.html wenn die IP-Adresse ausgewählt ist
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Jeder HTML-Taster ergibt einen Nummer zurück
+
+@app.route('/<changepin>', methods=['POST'])
+def reroute(changepin):
+    changePin = int(changepin)  # cast changepin to an int
+
+    if changePin == 1:
+        print("Boiler")  # Relais1 AN
+        GPIO.output(pin1, 0)  # Low-Pegel
+
+    elif changePin == 2:
+        print("Heizung")  # Relais2 AN
+        GPIO.output(pin2, 0)
+
+    else:
+        print("Off")  # Alle Relais AUS
+        GPIO.output(pin1, 1)
+        GPIO.output(pin2, 1)
+
+    response = make_response(redirect(url_for('index')))
+
+    return (response)
+
+
+app.run(debug=True, host='0.0.0.0', port=8000)  # Einrichten des Servers im Debug-Modus für Port 8000
